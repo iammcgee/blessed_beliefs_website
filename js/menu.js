2b18e28973199be7a80c4d1cfb20cb2e8db54edd
iammcgee <72765709+iammcgee@users.noreply.github.com>
2025-06-27 15:50:52 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toggle = document.getElementById('menu-toggle');
   const nav = document.getElementById('nav-menu');
-  
+
   toggle.addEventListener('click', function () {
     nav.classList.toggle('active');
   });
 });
+
